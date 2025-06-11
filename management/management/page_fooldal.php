9bb573d367b906e91a3e641bcf6514640a36aa00
DepoLeper <vogl.gergo@t-depo.hu>
2025-06-11 08:31:21 +0200

    Delete management directory

--- management/management/page_fooldal.php
+++
@@ -1,4 +0,0 @@
-<div class="bg-white p-6 rounded-xl shadow-lg">
-    <h3 class="text-lg font-semibold text-gray-700">Személyre szabható főoldal</h3>
-    <p class="mt-2 text-gray-600">Ez a felület a jövőben személyre szabható lesz, ide helyezheted ki a számodra legfontosabb KPI mutatókat a többi menüpontból.</p>
-</div>
--- management/page_termekek.php
+++
@@ -1,4 +0,0 @@
-<div class="bg-white p-6 rounded-xl shadow-lg">
-    <h3 class="text-lg font-semibold text-gray-700">Termékek KPI-ok</h3>
-    <p class="mt-2 text-gray-600">Itt fognak megjelenni a termékek teljesítményével kapcsolatos mutatók.</p>
-</div>
